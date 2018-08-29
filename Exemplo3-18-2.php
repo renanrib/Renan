cb001e1a5c2620f00bfabf5cac535fe3a60d4f66
Renan Ribeiro Silva <renanribsilva@gmail.com>
2018-08-29 17:13:45 -0300

    dia 29

---
+++ Exemplo3-18-2.php
@@ -0,0 +1,23 @@
+<?php
+
+$nome = "joão ranger";
+
+$nome = strtoupper($nome); // strtoupper => Maiuscula strtolower
+
+echo $nome;
+
+echo "<br>";
+
+$nome = strtolower($nome); // strtoupper => Maiuscula strtolower
+
+echo $nome;
+
+echo "<br>";
+
+echo  ucwords($nome);
+
+echo "<br>";
+
+echo ucfirst($nome);
+
+?>
---
+++ Exemplo3-19-2.php
@@ -0,0 +1,12 @@
+<?php
+
+//include "Exemplo3-19-1.php"; // "../inc/Example3-19-41"
+
+require_once "Exemplo3-19-1.php"; //o programa tem estar 100%, se não da erro fatal.
+require_once "Exemplo3-19-1.php";
+
+$resultado = soma(10, 30);
+
+echo $resultado;
+
+?>
